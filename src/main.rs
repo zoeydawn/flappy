203711c0f3f24f00237b00e784358b0c72500984
zoey <z@zoey.ninja>
2022-04-07 11:50:01 -0500

    added bracket_lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls(); // clears the window
+        ctx.print(1, 1, "Hello, Bracket Terminal!"); // prints text to the game window
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Flappy Dragon").build()?;
+    main_loop(context, State{})
 }
